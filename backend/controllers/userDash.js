e40551a95a8825e1d37b1b0a309f27232b99252d
Shadman Sohel <shadman.sohel04@gmail.com>
2024-06-30 01:03:43 -0400

    new Change

--- backend/controllers/userDash.js
+++ backend/controllers/userDash.js
@@ -25,7 +25,7 @@ const switchLeader = async (req, res)=>{
 
         return res.status(200).send({success: true, newLeader: msg})
     } catch (error) {
-        console.log(error)
+        return res.status(400).send({success: false, msg: error})
     }
 }
 

8b6d50a6d31b07bcc3e501e82145eb8ab4bf951f
Shadman Sohel <shadman.sohel04@gmail.com>
2025-07-22 18:05:25 -0400

    first commit

--- backend/app.js
+++ backend/app.js
@@ -32,20 +32,20 @@ app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
 
+const webs = ['https://rateto-backend.onrender.com/api/data?address=46.91310139948322+9.822978973388674&schoolChoice=private', 'https://leetbotbackend.onrender.com/gettheScores', 'https://hackthe6repo-ch8f.onrender.com/home/getRecipes', 'https://tmrun-h224.onrender.com/userdash/home/66c3f36f5a2adf3a2f754e27/scores']
+
+
 const pingWebsite = async () => {
-  try {
-    const response = await axios.get('https://rateto-backend.onrender.com/api/data?address=46.91310139948322+9.822978973388674&schoolChoice=private');
-    const response2 = await axios.get('https://leetbotbackend.onrender.com/gettheScores');
-    const response3 = await axios.get('https://hackthe6repo-ch8f.onrender.com/home/getRecipes');
-    const response4 = await axios.get('https://tmrun-h224.onrender.com/userdash/home/66c3f36f5a2adf3a2f754e27/scores');
-    
-    console.log(`Ping successful: ${response.status}`);
-    console.log(`Ping2 successful: ${response2.status}`);
-    console.log(`Ping3 successful: ${response3.status}`);
-    console.log(`Ping4 successful: ${response4.status}`);
-  } catch (error) {
-    console.error(`Ping failed: ${error.message}`);
-  }
+  await Promise.all(
+    webs.map(async (each) => {
+      try {
+        const response = await axios.get(each);
+        console.log(`Success: ${each}`);
+      } catch (error) {
+        console.log(`Failed: ${each}`);
+      }
+    })
+  );
 };
 
 

688697169e577638dd595ee61a232b315592aada
Shadman Sohel <shadman.sohel04@gmail.com>
2025-07-22 21:43:43 -0400

    backend leetbot

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
 
-const webs = ['https://rateto-backend.onrender.com/api/data?address=46.91310139948322+9.822978973388674&schoolChoice=private', 'https://leetbotbackend.onrender.com/gettheScores', 'https://hackthe6repo-ch8f.onrender.com/home/getRecipes', 'https://tmrun-h224.onrender.com/userdash/home/66c3f36f5a2adf3a2f754e27/scores']
+const webs = ['https://rateto-backend.onrender.com/api/data?address=46.91310139948322+9.822978973388674&schoolChoice=private', 'https://livechatproject-410t.onrender.com/gettheScores', 'https://hackthe6repo-ch8f.onrender.com/home/getRecipes', 'https://tmrun-h224.onrender.com/userdash/home/66c3f36f5a2adf3a2f754e27/scores']
 
 
 const pingWebsite = async () => {

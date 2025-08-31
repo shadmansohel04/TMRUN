284a0e333b1d61abff92e04d00b4c72604e70a24
Shadman Sohel <shadman.sohel04@gmail.com>
2025-08-31 14:24:55 -0400

    added StreakMS

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,14 @@ app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
 
-const webs = ['https://rateto-backend.onrender.com/api/data?address=46.91310139948322+9.822978973388674&schoolChoice=private', 'https://livechatproject-410t.onrender.com/gettheScores', 'https://hackthe6repo-ch8f.onrender.com/home/getRecipes', 'https://tmrun-h224.onrender.com/userdash/home/66c3f36f5a2adf3a2f754e27/scores', 'https://foodskitest.onrender.com/profile/likedCards']
+const webs = [
+  'https://rateto-backend.onrender.com/api/data?address=46.91310139948322+9.822978973388674&schoolChoice=private', 
+  'https://livechatproject-410t.onrender.com/gettheScores', 
+  'https://hackthe6repo-ch8f.onrender.com/home/getRecipes', 
+  'https://tmrun-h224.onrender.com/userdash/home/66c3f36f5a2adf3a2f754e27/scores', 
+  'https://foodskitest.onrender.com/profile/likedCards',
+  "https://streakms-jni8.onrender.com/"
+]
 
 const pingWebsite = async () => {
   await Promise.all(

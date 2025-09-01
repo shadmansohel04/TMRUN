2091adee794fd166a45e70f0a4c383d1a52125a5
Shadman Sohel <shadman.sohel04@gmail.com>
2025-09-01 13:28:18 -0400

    cleaned up spin up logic

--- backend/app.js
+++ backend/app.js
@@ -33,22 +33,30 @@ app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 3000;
 
 const webs = [
-  'https://rateto-backend.onrender.com/api/data?address=46.91310139948322+9.822978973388674&schoolChoice=private', 
-  'https://livechatproject-410t.onrender.com/gettheScores', 
-  'https://hackthe6repo-ch8f.onrender.com/home/getRecipes', 
-  'https://tmrun-h224.onrender.com/userdash/home/66c3f36f5a2adf3a2f754e27/scores', 
-  'https://foodskitest.onrender.com/profile/likedCards',
-  "https://streakms-jni8.onrender.com/"
+  {url:'https://rateto-backend.onrender.com/api/data?address=46.91310139948322+9.822978973388674&schoolChoice=private', method: "GET"}, 
+  {url:'https://livechatproject-410t.onrender.com/gettheScores', method: "GET"},
+  {url:'https://hackthe6repo-ch8f.onrender.com/home/getRecipes', method: "GET"},
+  {url:'https://tmrun-h224.onrender.com/userdash/home/66c3f36f5a2adf3a2f754e27/scores', method:"GET"},
+  {url: 'https://foodskitest.onrender.com/recipe/ParseRecipe', method: "POST", body: {Recipe: "Mix in the avocado with the milk"}},
+  {url:"https://streakms-jni8.onrender.com/schedule/", method: "GET"}
 ]
 
 const pingWebsite = async () => {
   await Promise.all(
     webs.map(async (each) => {
       try {
-        const response = await axios.get(each);
-        console.log(`Success: ${each}`);
+        let response
+        if(each.method == "GET"){
+          response = await axios.get(each.url);
+          // console.log(`Success: ${each.url}: ${response.status}`);
+        }
+        else if (each.method == "POST"){
+          response = await axios.post(each.url, each.body);
+          console.log(`Success: ${each.url}: ${response.status}`);
+        }
+
       } catch (error) {
-        console.log(`Failed: ${each}`);
+        console.log(`Failed: ${each.url}: ${error}`);
       }
     })
   );

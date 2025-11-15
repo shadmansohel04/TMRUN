51bbab697df2ad2cb6cf81a436322712e5efac20
Shadman Sohel <shadman.sohel04@gmail.com>
2025-11-15 12:36:03 -0500

    bring back ip

--- backend/controllers/main.js
+++ backend/controllers/main.js
@@ -1,6 +1,5 @@
 const CustomAPIError = require('../errors/custom-error')
 const transporter = require('../models/mailer')
-const axios = require("axios")
 
 const homeGet = async(req, res)=>{
     res.status(200).json({msg:"on the home page"})
@@ -10,54 +9,37 @@ const aboutGet = async (req, res)=>{
     res.status(200).send('yayayaya')
 }
 
-const sendContact = async (req, res) => {
-  try {
-    const ip = req.headers['x-forwarded-for']?.split(',')[0]?.trim() || req.socket.remoteAddress;
+const sendContact = async(req, res)=>{
+    try {
+        const {email, subject, message} = req.body
 
-    const location = await axios.get(`https://ipapi.co/${ip}/json/`)
-      .then(r => r.data)
-      .catch(() => null);
+        const ip = req.ip || "Bum Bum"
+        let location
+        try {
+            const res = await fetch(`https://ipapi.co/${ip}/json/`);
+            const data = await res.text();
+            location = data
+        } catch (error) {
+            location = "idk";
+        }
 
-    await transporter.sendMail({
-      from: req.body.email,
-      to: process.env.CONTACT_EMAIL,
-      subject: "New Contact Form Submission",
-      text: `
-Name: ${req.body.name}
-Email: ${req.body.email}
-Message: ${req.body.message}
-
----- IP INFORMATION ----
-IP Address: ${ip}
-Location: ${location ? JSON.stringify(location, null, 2) : "Unavailable"}
-      `,
-      html: `
-        <p><strong>Name:</strong> ${req.body.name}</p>
-        <p><strong>Email:</strong> ${req.body.email}</p>
-        <p><strong>Message:</strong><br>${req.body.message}</p>
-
-        <hr>
-        <h3>IP Information</h3>
-        <p><strong>IP Address:</strong> ${ip}</p>
-        <p><strong>Location:</strong></p>
-        <pre>${location ? JSON.stringify(location, null, 2) : "Unavailable"}</pre>
-      `
-    });
-
-    res.status(200).send({
-      success: true,
-      ip,
-      location
-    });
-  } catch (error) {
-    console.log(error)
-    res.status(200).send({ 
-        success: false, 
-        error: error
-    });
-  }
-};
-
+        transporter.sendMail({
+            from: 'tmrun.mail.co@gmail.com',
+            to: 'shadman.sohel04@gmail.com',
+            subject: subject,
+            text: email + " \n" + message + "\n" + ip + "\n" + location
+        }, (error, info) => {
+            if (error) {
+                return next(new CustomAPIError(400, 'Proper email not given'));
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        })
+        res.status(200).send({success: true})
+    } catch (error) {
+        res.status(200).send({success: false})
+    }
+}
 
 module.exports = {
     homeGet,
